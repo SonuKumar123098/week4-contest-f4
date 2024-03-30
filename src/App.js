0964b9a195eda021acf50db3fea4ccd2aae6de4b
Sonu Kumar <sonukrsingh998@gmail.com>
2024-03-30 23:39:36 +0530

    l

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
-import RoutesManager from "./routes/Routes";
-import Loader from "./components/Loader";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./Components/Homepage";
+import Item from "./Components/Item";
+import Navbar from "./Components/Navbar";
+import Loader from "./Components/Loader";
 
 const App = () => {
+  const [selectedItem, setSelectedItem] = useState({});
   return (
-    <>
-      <RoutesManager />
-    </>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route
+          path="/"
+          element={<Homepage setSelectedItem={setSelectedItem} />}
+        />
+        <Route
+          path={`/item/:${selectedItem.id}`}
+          element={<Item selectedItem={selectedItem} setSelectedItem={setSelectedItem}/>}
+        />
+        <Route
+          path="/notifications"
+          element={<Loader/>}
+        />
+        <Route
+          path="/item/"
+          element={<Item selectedItem={selectedItem} setSelectedItem={setSelectedItem}/>}
+        />
+        <Route
+          path="/profile"
+          element={<Loader/>}
+        />
+      </Routes>
+    </div>
   );
 };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
-import { thunk } from "redux-thunk";
-import reducer from "./reducer";
-const store = createStore(reducer, applyMiddleware(thunk));
+// store.js
+import { legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux';
+import { thunk } from 'redux-thunk';
+import rootReducer from './Reducers/reducer';
+
+// const rootreducer = combineReducers({rootReducer});   // this line will be used when there multiple reducers.
+
+// Create the Redux store with reducers and middleware
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 export default store;
